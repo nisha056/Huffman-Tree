862afeefce538cc1afa3f5dfa0d0d0c7c67163c7
Nisha Sharma <60255047+nisha056@users.noreply.github.com>
2020-12-08 15:33:17 +0545

    Update Textbox.h

--- Textbox.h
+++ Textbox.h
@@ -11,145 +11,149 @@
 
 class Textbox
 {
-    public:
-        Textbox();
-        Textbox(int size,sf::Color color,bool sel)
+public:
+
+    sf::RectangleShape outline;
+    Textbox();
+
+    Textbox(int size, sf::Color color, bool sel)
+    {
+        outline.setSize(sf::Vector2f(100, size * 1.5));
+        outline.setOutlineThickness(5);
+        outline.setOutlineColor(sf::Color::Black);
+        textbox.setCharacterSize(size);
+        textbox.setFillColor(color);
+        isSelected = sel;
+        if (sel)
         {
-            textbox.setCharacterSize(size);
-            textbox.setColor(color);
-            isSelected = sel;
-            if (sel)
-            {
-                textbox.setString(" _ ");
-            }
-            else{
-                textbox.setString("");
-            }
+            textbox.setString("_");
+        }
+        else {
+            textbox.setString("");
+        }
 
 
+    }
+    bool isMouseOver(sf::RenderWindow& window) {
+        int mouseX = sf::Mouse::getPosition(window).x;
+        int mouseY = sf::Mouse::getPosition(window).y;
+
+        int btnPosX = outline.getPosition().x;
+        int btnPosY = outline.getPosition().y;
+
+        int btnxPosWidth = outline.getPosition().x + outline.getSize().x;
+        int btnyPosHeight = outline.getPosition().y + outline.getSize().y;
+
+        if (mouseX < btnxPosWidth && mouseX > btnPosX && mouseY < btnyPosHeight && mouseY > btnPosY) {
+            return true;
         }
-        void setFont(sf::Font &font)
+        return false;
+    }
+    void setFont(sf::Font& font)
+    {
+        textbox.setFont(font);
+    }
+    void setPosition(sf::Vector2f pos)
+    {
+        outline.setPosition(pos);
+        textbox.setPosition(pos);
+    }
+    void setLimit(bool ToF)
+    {
+        hasLimit = ToF;
+    }
+    void setLimit(bool ToF, int lim)
+    {
+        hasLimit = ToF;
+        limit = lim - 1;
+        outline.setSize(sf::Vector2f(lim * 11, outline.getSize().y));
+    }
+    void setSelected(bool sel)
+    {
+        isSelected = sel;
+        if (!sel)
         {
-            textbox.setFont(font);
-        }
-        void setPosition(sf::Vector2f pos)
-        {
-            textbox.setPosition(pos);
-        }
-        void setLimit(bool ToF)
-        {
-            hasLimit= ToF;
-        }
-        void setLimit(bool ToF,int lim)
-        {
-            hasLimit = ToF;
-            limit = lim;
-        }
-        void setSelected(bool sel)
-        {
-            isSelected = sel;
-            if(!sel)
-            {
-            std::string t= text.str();
+            std::string t = text.str();
             std::string newT = "";
-            for(int i=0;i< t.length ();i++)
+            for (int i = 0; i < (int)t.length(); i++)
             {
                 newT += t[i];
             }
             textbox.setString(newT);
 
-            }
         }
-        std::string getInput()
+    }
+    std::string getInput()
+    {
+        return text.str();
+    }
+    void drawTo(sf::RenderWindow& window)
+    {
+
+        window.draw(outline);
+        window.draw(textbox);
+    }
+    void typedOn(sf::Event input)
+    {
+        if (isSelected)
         {
-            return text.str();
-        }
-        void drawTo(sf::RenderWindow &window)
-        {
-            window.draw(textbox);
-        }
-        void typedOn(sf::Event input)
-        {
-            if(isSelected)
+            int charTyped = input.text.unicode;
+            if (charTyped < 128)
             {
-                int charTyped = input.text.unicode;
-                if(charTyped <128)
+                if (hasLimit)
                 {
-                    if (hasLimit)
-                    {
-                        if(text.str().length()<= limit)
-                        {
-                            inputLogic(charTyped);
-                        }
-                        else if(text.str().length()<= limit && charTyped == DELETE_KEY)
-                        {
-                            deletelastChar();
-                        }
-                    }
-                    else
+                    if ((int)text.str().length() <= limit)
                     {
                         inputLogic(charTyped);
-
+                    }
+                    else if ((int)text.str().length() <= limit && charTyped == DELETE_KEY)
+                    {
+                        deletelastChar();
                     }
                 }
-            }
-        }
-        void print()
-        {
-            int characterFrequency [128];
-    std::string t= getInput();
-    std::vector<char> arr;
-   // size_t size = arr.size();
-    std::vector<int> freq;
-    for(int i=0 ;i<128;i++){
-        characterFrequency[i] = 0;
-    }
-    for(int i=0;t.length();i++){
-        characterFrequency[(int)t[i]]++;
-    }
-    size_t size = t.size();
-    for(int i=0 ;i<128;i++){
-        if(characterFrequency[i]>0){
-            arr.push_back((char)i);
-            freq.push_back(characterFrequency[i]);
-        }
-    }
-        }
-
-    private:
-        sf::Text textbox;
-        std::ostringstream text;
-        bool isSelected = false;
-        bool hasLimit = false;
-        int limit;
-        void inputLogic(int charTyped)
-        {
-            if(charTyped != DELETE_KEY && charTyped != ENTER_KEY && charTyped != ESCAPE_KEY)
-            {
-                text << static_cast<char>(charTyped);
-            }
-            else if(charTyped == DELETE_KEY)
-            {
-                if(text.str().length() >0)
+                else
                 {
-                    deletelastChar();
+                    inputLogic(charTyped);
+
                 }
             }
-            textbox.setString(text.str() + "_");
+        }
+    }
 
-        }
-        void deletelastChar()
+private:
+    sf::Text textbox;
+    std::ostringstream text;
+    bool isSelected = false;
+    bool hasLimit = false;
+    int limit;
+    void inputLogic(int charTyped)
+    {
+        if (charTyped != DELETE_KEY && charTyped != ENTER_KEY && charTyped != ESCAPE_KEY)
         {
-            std::string t= text.str();
-            std::string newT = "";
-            for(int i=0;i< t.length ()-1;i++)
-            {
-                newT += t[i];
-            }
-            text.str("");
-            text<< newT;
-            textbox.setString(text.str());
+            text << static_cast<char>(charTyped);
         }
+        else if (charTyped == DELETE_KEY)
+        {
+            if (text.str().length() > 0)
+            {
+                deletelastChar();
+            }
+        }
+        textbox.setString(text.str() + "_");
+
+    }
+    void deletelastChar()
+    {
+        std::string t = text.str();
+        std::string newT = "";
+        for (int i = 0; i < (int)t.length() - 1; i++)
+        {
+            newT += t[i];
+        }
+        text.str("");
+        text << newT;
+        textbox.setString(text.str());
+    }
 };
 
 #endif // TEXTBOX_H
