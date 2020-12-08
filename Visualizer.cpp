fc77412c4eeee80af88b8039f96dcc956b84dd59
Nisha Sharma <60255047+nisha056@users.noreply.github.com>
2020-12-08 12:15:30 +0545

    Update Visualizer.cpp

--- Visualizer.cpp
+++ Visualizer.cpp
@@ -1,25 +1,39 @@
 #include "Visualizer.h"
-Visualizer::Visualizer():window(sf::VideoMode(1000,900),"Visualizer"),
+Visualizer::Visualizer() :window(sf::VideoMode(1000, 900), "Visualizer"),
 
-setDataButton("Set",sf::Vector2f(120,50),20,sf::Color::Green,sf::Color::Black),
-treeButton("Tree",sf::Vector2f(120,50),20,sf::Color::Green,sf::Color::Black ) ,
-textbox1(30,sf::Color::Red,false)
+setDataButton("Set", sf::Vector2f(120, 50), 20, sf::Color::Green, sf::Color::Black),
+treeButton("Tree", sf::Vector2f(120, 50), 20, sf::Color::Green, sf::Color::Black),
+boxButton(" ", sf::Vector2f(400, 35), 20, sf::Color::White, sf::Color::Cyan), textbox1(20, sf::Color::Red, false)
 {
-    if(!font.loadFromFile("DejaVuSans.ttf"))
+    if (!font.loadFromFile("DejaVuSans.ttf"))
     {
         //error
     }
-    setDataButton.setPosition(sf::Vector2f(55,55));
+    setDataButton.setPosition(sf::Vector2f(55, 55));
     setDataButton.setFont(font);
-    treeButton.setPosition(sf::Vector2f(825,55));
+    treeButton.setPosition(sf::Vector2f(825, 55));
     treeButton.setFont(font);
+    boxButton.setPosition(sf::Vector2f(380, 80));
+    boxButton.setFont(font);
     textbox1.setFont(font);
-    textbox1.setPosition(sf::Vector2f(400,70));
-    }
+    textbox1.setPosition(sf::Vector2f(200, 80));
+    textbox1.setLimit(true, 50);// 22 characters can be entered
+    OutlineBox1.setSize(sf::Vector2f(900, 800));
+    OutlineBox1.setPosition(sf::Vector2f(50, 50));
+    OutlineBox1.setFillColor(sf::Color::Transparent);
+    OutlineBox1.setOutlineColor(sf::Color::Black);
+    OutlineBox1.setOutlineThickness(10);
+    drawingAreaOutline.setSize(sf::Vector2f(900, 600));
+    drawingAreaOutline.setPosition(sf::Vector2f(50, 150));
+    drawingAreaOutline.setFillColor(sf::Color::Transparent);
+    drawingAreaOutline.setOutlineColor(sf::Color::Black);
+    drawingAreaOutline.setOutlineThickness(10);
+    H.setDrawingArea(drawingAreaOutline);
+}
 
-void Visualizer:: run()
+void Visualizer::run()
 {
-    while(window.isOpen())
+    while (window.isOpen())
     {
         handleUserInput();
         render();
@@ -27,40 +41,31 @@ void Visualizer:: run()
 }
 void Visualizer::handleUserInput()
 {
+    sf::Vector2f mousePosition =sf::Vector2f (sf::Mouse::getPosition().x, sf::Mouse::getPosition().y);
     sf::Event event;
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
+    while (window.pollEvent(event))
     {
-        textbox1.setSelected(true);
-    }
-    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-    {
-        textbox1.setSelected(false);
-    }
-     while(window.pollEvent(event))
-    {
-        switch(event.type)
+        switch (event.type)
         {
         case sf::Event::Closed:
             window.close();
             break;
-        //if (event.type== sf::Event::Closed)
-           // window.close();
         case sf::Event::TextEntered:
             textbox1.typedOn(event);
             break;
-          case sf::Event::EventType::MouseMoved:
+        case sf::Event::EventType::MouseMoved:
         {
-            if(setDataButton.isMouseOver(window))
+            if (setDataButton.isMouseOver(window))
             {
                 setDataButton.setBackColor(sf::Color::Red);
 
             }
-           else
+            else
             {
                 setDataButton.setBackColor(sf::Color::Green);
             }
 
-            if(treeButton.isMouseOver(window))
+            if (treeButton.isMouseOver(window))
             {
                 treeButton.setBackColor(sf::Color::Red);
             }
@@ -72,39 +77,38 @@ void Visualizer::handleUserInput()
         }
         case  sf::Event::EventType::MouseButtonPressed:
         {
-              if (setDataButton.isMouseOver(window))
-              {
-                  H.setData( );
+            if (textbox1.isMouseOver(window))
+            {
+                textbox1.setSelected(true);
+            }
+            else if (setDataButton.isMouseOver(window))
+            {
+               // H.setData();
 
-              }
-              else if (treeButton.isMouseOver(window))
-              {
+            }
+            else if (treeButton.isMouseOver(window))
+            {
+                H.run(textbox1.getInput());
+            }
+            else
+            {
+                textbox1.setSelected(false);
+            }
+        }
+        break;
+        }
 
-              }
-          }
-          break;
-          }
-
-}
+    }
 }
 void Visualizer::render()
 {
     window.clear(sf::Color::White);
     setDataButton.drawTo(window);
     treeButton.drawTo(window);
+    boxButton.drawTo(window);
     textbox1.drawTo(window);
+    window.draw(OutlineBox1);
+    window.draw(drawingAreaOutline);
     H.draw(window);
     window.display();
 }
-/*case sf::Event::TextEntered:
-			if ((event.text.unicode >= 48 && event.text.unicode <= 57) || (event.text.unicode == 46))
-			{
-				inputText += event.text.unicode;
-				playerInput.setString(inputText);
-			}
-			else if (event.text.unicode == 8 && !inputText.isEmpty())
-			{
-				inputText.erase(inputText.getSize() - 1, 1);
-				playerInput.setString(inputText);
-			}
-			break;*/
